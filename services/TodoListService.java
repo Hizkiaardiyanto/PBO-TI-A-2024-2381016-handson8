1e41d72aa8cc63735855e6f0351f8521539fe047
AditiaGA <hss69888@gmail.com>
2024-11-08 14:35:12 +0700

    Handson8

---
+++ services/TodoListService.java
@@ -0,0 +1,10 @@
+package services;
+
+import entities.TodoList;
+
+public interface TodoListService {
+    TodoList[] getTodoList();
+    void AddTodolist(String todo);
+    Boolean removeTodoList(Integer number);
+    Boolean editTodoList(Integer number, String todo);
+}
