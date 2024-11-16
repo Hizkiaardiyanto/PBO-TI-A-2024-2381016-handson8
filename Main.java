218a8edcd8d4267f35348809477d1fb71c078be2
AditiaGA <hss69888@gmail.com>
2024-11-17 00:47:55 +0700

    Handson9

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import Views.TodoListTerminalView;
+import Views.TodoListView;
+import config.Database;
+import repositories.TodoListRepository;
+import repositories.TodoListRepositoryDbImpl;
+import services.TodoListService;
+import services.TodoListServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Database database = new Database("my_database", "root", "", "localhost", "3306");
+        database.setup();
+
+        TodoListRepository todoListRepository = new TodoListRepositoryDbImpl(database);
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListTerminalView(todoListService);
+        todoListView.run();
     }
 }
